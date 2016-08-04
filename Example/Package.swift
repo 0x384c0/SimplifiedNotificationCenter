d072cfdefd7c12b74c81b176c96bf0b3f409288a
Andrew Ashurow <andrew@Helvetica-Standard.local>
2016-08-04 11:48:06 +0600

    added Package.swift

---
+++ Example/Package.swift
@@ -0,0 +1,20 @@
+//
+//  Package.swift
+//  SimplifiedNotificationCenter
+//
+//  Created by Andrew Ashurow on 04.08.16.
+//  Copyright © 2016 CocoaPods. All rights reserved.
+//
+
+
+let
+name = "SimplifiedNotificationCenter",
+git = "https://github.com/0x384c0/SimplifiedNotificationCenter.git"
+
+//import PackageDescription
+//let package = Package(
+//    name: name,
+//    dependencies: [
+//        .Package(url: git, majorVersion: 1),
+//    ]
+//)
